28d830c503e9584df570da086cad81541131ec4b
Cristopher Hernández <73862045+Cristopher2874@users.noreply.github.com>
2024-09-30 22:20:24 -0600

    squaresI

--- C++/leetcode/easy/squares.cpp
+++ C++/leetcode/easy/squares.cpp
@@ -4,11 +4,10 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> squares(nums.size());
         for(int i=0;i<nums.size();i++){
-            squares.push_back(nums[i]*nums[i]);
-            squares.insert(squares.begin(),nums[i]*nums[i]);
+            nums[i]=nums[i]*nums[i];
         }
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
@@ -16,7 +15,8 @@ public:
 int main(){
     vector<int> nums = {12,345,23,21,4,23,45,-2};
     Solution s;
-    for(int square:s.sortedSquares(nums)){
+    vector<int> num2 = s.sortedSquares(nums);
+    for(int square:num2){
         cout<<square<<" ";
     }
     return 0;
