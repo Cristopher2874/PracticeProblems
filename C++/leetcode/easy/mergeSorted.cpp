56145ea7c2371815d180bb86a02093d4f93249af
Cristopher Hernández <73862045+Cristopher2874@users.noreply.github.com>
2024-10-10 13:05:58 -0600

    merge arrays

--- C++/leetcode/easy/mergeSorted.cpp
+++ C++/leetcode/easy/mergeSorted.cpp
@@ -4,15 +4,26 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+        m--;
+        n--;
+        while(n>=0){
+            while(nums1[m]>=nums2[n]&&m>=0&&n>=0){
+                m--;
+            }
+            while(nums1[m]<=nums2[n]&&m>=0&&n>=0){
+                nums1.insert(nums1.begin()+m+1,nums2[n]);
+                nums1.pop_back();
+                n--;
+            }
+        }
     }
 };
 
 int main(){
-    vector<int> nums = {1,7,7,9,0,0};
-    vector<int> nums2 = {2,5};
+    vector<int> nums = {1,7,0,0,0,0};
+    vector<int> nums2 = {2,3,5,10};
     Solution s;
-    s.merge(nums,4,nums2,2);
+    s.merge(nums,2,nums2,4);
     for(auto square:nums){
         cout<<square<<" ";
     }
