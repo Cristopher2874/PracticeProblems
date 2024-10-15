51e39cba061aad5f413dfc91a7f12db685bc3b6d
Cristopher Hernández <73862045+Cristopher2874@users.noreply.github.com>
2024-10-14 18:35:08 -0600

    final topics

---
+++ C++/leetcode/med/numberOfStrVowelCons.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countOfSubstrings(string word, int k) {
+        unordered_map<string,int> con;
+    }
+};
+
+int main(){
+    Solution s;
+    string word = "havefun";
+    int k = 3;
+    cout<<s.countOfSubstrings(word,k);
+    return 0;
+}
+
+//vps, aparte para uso personal
+//fast api
+//métodos http
+//códigos http
+//virtual env
