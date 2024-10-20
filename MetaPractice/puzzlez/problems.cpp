f08edf294f328d86e144ba11710293e97bffdef2
Cristopher Hernández <73862045+Cristopher2874@users.noreply.github.com>
2024-10-20 14:33:32 -0600

    problemas 2

---
+++ MetaPractice/puzzlez/problems.cpp
@@ -0,0 +1,20 @@
+/*
+ 1. Given two strings with integers, add the sum:
+ "99", "111"
+ sum = "11010"
+3. Given two arrays with events, and the capacity of the server,:
+"99", "111"
+"REQUEST", "FAIL 2"
+Return the index of the server with most requests, with the rules:
+- If the server has received N request, it is full, move to the next server}
+- If there is FAIL 2, the server 2 is no longer considered for use
+- If all the servers are full, then all the servers are reset and you have to start in index 0
+- Large contrains 10*5
+4. Given an array of strings, with obstacles placed as:
+[1, 5] (obstacle at 5)
+or pieces in 
+[2,4,2], see biuld piece in 4 with size of 1 per side
+Longer constrains
+Return and string "10101", if the piece could be built, in case it does not encounter any obstalce
+
+*/
