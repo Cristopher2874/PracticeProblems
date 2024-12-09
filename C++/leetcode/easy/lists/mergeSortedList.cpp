6bf2f85531b0c4a52251be5c66fa584c81516bed
Main <cris.main27@gmail.com>
2024-12-08 21:00:44 -0600

    order cpp files

---
+++ C++/leetcode/easy/lists/mergeSortedList.cpp
@@ -0,0 +1,44 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Input: list1 = [], list2 = []
+Output: []
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head = new ListNode(1);
+        
+        return head;
+    }
+};
+
+int main(){
+    ListNode* m1 = new ListNode(1);
+    ListNode* m2 = new ListNode(1);
+    Solution s;
+    ListNode* sol = s.mergeTwoLists(m1,m2);
+    return 0;
+}
