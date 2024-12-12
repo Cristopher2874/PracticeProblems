93fb6380f85f58ae401239ca4574294d6bad3460
Main <cris.main27@gmail.com>
2024-12-12 13:21:32 -0600

    add First occurrency Str

---
+++ C++/leetcode/easy/String/FirstOccurrencyStr.cpp
@@ -0,0 +1,33 @@
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+    }
+};
+
+int main(){
+    return 0;
+}
