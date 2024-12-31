d5e93b2b443a8eb293f3eb14a3b407b09beb76c3
Cristopher2874 <cris.main27@gmail.com>
2024-12-30 22:09:56 -0600

    Daily problem 30/12/2024

--- C++/leetcode/easy/String/LengthLastWord.cpp
+++ C++/leetcode/easy/String/LengthLastWord.cpp
@@ -1,7 +1,7 @@
 /*
 Given a string s consisting of words and spaces, return the length of the last word in the string.
 
-A word is a maximal 
+A word is a maximal
 substring
  consisting of non-space characters only.
 
@@ -16,7 +16,7 @@ Explanation: The last word is "moon" with length 4.
 Input: s = "luffy is still joyboy"
 Output: 6
 Explanation: The last word is "joyboy" with length 6.
- 
+
 Constraints:
 
 1 <= s.length <= 104
@@ -24,26 +24,36 @@ s consists of only English letters and spaces ' '.
 There will be at least one word in s.
 */
 
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class Solution{
-    public:
-    int lengthOfLastWord(string s) {
-        //search for a split method that splits the string by spaces and takes the last item
-        // problem, the lasst item could contain spaces
-        // iterate from the end until we find a non-space character
-        // continue iterate until we find a space character
-        // return the number of iterations
-        return s.length();
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        if (s.size() == 0)
+            return 0;
+        int i = s.size() - 1;
+        int len=0;
+        while (i >= 0 && s[i] == ' ')
+        {
+            --i;
+        }
+        while (i >= 0 && s[i] != ' ')
+        {
+            ++len;
+            --i;
+        }
+        return len;
     }
 };
 
-int main(){
+int main()
+{
     Solution s;
-    string str = "  Hello World   ";
-    cout<<s.lengthOfLastWord(str)<<endl;
+    string str = "";
+    cout << s.lengthOfLastWord(str) << endl;
     return 0;
 }
