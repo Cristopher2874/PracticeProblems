8482d2fb177d3e15c15fd70bdc5d412daae23fd4
Cristopher2874 <cris.main27@gmail.com>
2024-12-31 11:43:12 -0600

    Pre daily problem

---
+++ C++/leetcode/easy/Array/PlusOne.cpp
@@ -0,0 +1,68 @@
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+
+ARRAY MATH
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+    public:
+        vector<int> plusOne(vector<int>& digits){
+            int res=0;
+            for(size_t i=digits.size()-1;i>=0;i--){
+                if(i<0){
+                    digits.insert(digits.begin(),res);
+                    break;
+                }
+                int temp=digits[i]+1+res;
+                if(temp>=10){
+                    digits[i]=(temp%10);
+                    ++res;
+                }else{
+                    digits[i]=temp;
+                    break;
+                }
+                res=0;
+            }
+            return digits;
+        }
+};
+
+int main(){
+    Solution s;
+    vector<int> digits = {1,9,9,9,9,9,9};
+    vector<int> result = s.plusOne(digits);
+    for(size_t i=0;i<result.size();++i){
+        cout << result[i] << " ";
+    }
+    return 0;
+}
