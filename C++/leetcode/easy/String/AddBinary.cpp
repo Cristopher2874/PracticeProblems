0c8534387de734200652d205e40629ba959c134e
Main <cris.main27@gmail.com>
2025-01-08 14:35:10 -0600

    Add binary

--- C++/leetcode/easy/String/AddBinary.cpp
+++ C++/leetcode/easy/String/AddBinary.cpp
@@ -24,20 +24,17 @@ class Solution{
     public:
         string addBinary(string a, string b){
             string result = "";
-            int lastA=a.size()-1, lastB=b.size()-1, carry=0;
+            int lastA=a.size()-1, lastB=b.size()-1, carry=0, sum = 0;
+            int n=0,m=0;
             while(lastA>=0||lastB>=0){
-                int sum = carry;
-                if(lastA>=0 && lastB>=0){
-                    sum=sum^(a[lastA--]^b[lastB--]);
-                }else if(lastB>=0){
-                    sum=sum^(0^b[lastB--]);
-                }else{
-                    sum=sum^(a[lastA--]^0);
-                }
-
-                carry = sum&1;
-                result.push_back((sum+'0'));
+                n=lastA<0?0:(a[lastA]-'0');
+                m=lastB<0?0:(b[lastB]-'0');
+                sum = carry^n^m;
+                carry= (carry+n+m>=2) ? 1 : 0;
+                result.push_back((sum) ? '1' : '0');
                 sum=0;
+                --lastA;
+                --lastB;
             }
 
             /*
@@ -46,8 +43,8 @@ class Solution{
             0+1 = 1 carry 0
             0+0 = 0 carry 0
             */
-           result.reserve();
            if(carry) result.push_back('1');
+           reverse(result.begin(), result.end());
            return result;
         }
 };
